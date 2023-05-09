563b7914d5dffbb64673f0f080951de1b21aad47
kimbs <vlakd0711@naver.com>
2023-05-09 19:13:36 +0900

    Test: File, Notice 아키텍처 테스트 코드 추가

---
+++ src/test/java/clone/bzm/lounge/file/FileArchitectureTests.java
@@ -0,0 +1,28 @@
+package clone.bzm.lounge.file;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.library.Architectures.onionArchitecture;
+
+@AnalyzeClasses(packages = "clone.bzm.lounge.file")
+public class FileArchitectureTests {
+
+    @ArchTest
+    static final ArchRule testOnionArchitecture =
+            onionArchitecture()
+                    .domainModels("clone.bzm.lounge.file.domain..")
+                    .domainServices("clone.bzm.lounge.file.application.service..")
+                    .applicationServices(
+                            "clone.bzm.lounge.file.application..",
+                            "clone.bzm.lounge.common..",
+                            "clone.bzm.lounge.configration.."
+                    )
+                    .adapter("in-rest", "clone.bzm.lounge.file.adapter.in.rest..")
+                    .adapter("out-jpa", "clone.bzm.lounge.file.adapter.out.jpa..")
+                    .adapter("out-disk", "clone.bzm.lounge.file.adapter.out.disk..")
+                    .adapter("out-storage", "clone.bzm.lounge.file.adapter.out.storage..")
+            ;
+
+}
---
+++ src/test/java/clone/bzm/lounge/notice/NoticeArchitectureTests.java
@@ -0,0 +1,26 @@
+package clone.bzm.lounge.notice;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.library.Architectures.onionArchitecture;
+
+@AnalyzeClasses(packages = "clone.bzm.lounge.notice")
+public class NoticeArchitectureTests {
+
+    @ArchTest
+    static final ArchRule testOnionArchitecture =
+            onionArchitecture()
+                    .domainModels("clone.bzm.lounge.notice.domain..")
+                    .domainServices("clone.bzm.lounge.notice.application.service..")
+                    .applicationServices(
+                            "clone.bzm.lounge.notice.application..",
+                            "clone.bzm.lounge.common..",
+                            "clone.bzm.lounge.configration.."
+                    )
+                    .adapter("in-rest", "clone.bzm.lounge.notice.adapter.in.rest..")
+                    .adapter("out-jpa", "clone.bzm.lounge.notice.adapter.out.jpa..")
+            ;
+
+}
