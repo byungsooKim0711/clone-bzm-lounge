a8b1107be51247b49070ad9f07513501564a1bc1
kimbs <vlakd0711@naver.com>
2023-05-22 17:42:30 +0900

    Fix: 누락된 import 추가

--- temp/src/test/java/clone/bzm/lounge/example/adapter/out/persistence/ExamplePersistenceAdapterTests.java
+++ temp/src/test/java/clone/bzm/lounge/example/adapter/out/persistence/ExamplePersistenceAdapterTests.java
@@ -10,6 +10,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
--- temp/src/test/java/clone/bzm/lounge/example/application/service/ExampleServiceTests.java
+++ temp/src/test/java/clone/bzm/lounge/example/application/service/ExampleServiceTests.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
