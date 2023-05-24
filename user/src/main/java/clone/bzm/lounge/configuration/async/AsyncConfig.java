0f92e140a0f86b19b9de3eaf1dccc2f66b251fb2
kimbs <vlakd0711@naver.com>
2023-05-24 16:17:51 +0900

    Add: async, jpa 설정 추가

---
+++ user/src/main/java/clone/bzm/lounge/configuration/async/AsyncConfig.java
@@ -0,0 +1,9 @@
+package clone.bzm.lounge.configuration.async;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+}
