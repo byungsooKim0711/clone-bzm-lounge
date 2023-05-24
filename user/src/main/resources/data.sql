c909bfc3ac11b1487537bb6152ce58fd0556d5b9
kimbs <vlakd0711@naver.com>
2023-05-24 16:18:12 +0900

    Add: 샘플 user 데이터 추가

--- user/src/main/resources/data.sql
+++ user/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 
 -- password: test
 insert into tb_user (id, email, name, password, phone_number, status, created_at, modified_at)
-values (null, 'test@test.com', 'test', '$2a$10$mhqOqjqu5373On8uXaUMA.TC7IiF0ifcbKkpc9xzcG31O9UObTfv2', '010-1234-5678', 'A', now(), now())
+values (null, 'test@test.com', 'test', '$2a$10$mhqOqjqu5373On8uXaUMA.TC7IiF0ifcbKkpc9xzcG31O9UObTfv2', '010-1234-5678', 'ACTIVE', now(), now())
 ;
